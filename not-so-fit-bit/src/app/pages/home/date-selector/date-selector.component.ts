7e8584734c59da009a81e40dae3780ce09f901a6
Kaden675 <kaden.heley@ndus.edu>
2025-04-24 15:59:28 -0500

    framework

---
+++ not-so-fit-bit/src/app/pages/home/date-selector/date-selector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-date-selector',
+  imports: [],
+  templateUrl: './date-selector.component.html',
+  styleUrl: './date-selector.component.css'
+})
+export class DateSelectorComponent {
+
+}
