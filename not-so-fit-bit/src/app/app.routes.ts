251ed7a53d614b1f64d528b730d1d6cdbebcc8ad
krinnels <kristopher.a.rinnels@ndscs.edu>
2025-04-26 16:54:44 -0500

    Added additional routes and routerLinks

--- not-so-fit-bit/src/app/app.routes.ts
+++ not-so-fit-bit/src/app/app.routes.ts
@@ -2,11 +2,18 @@ import { Routes } from '@angular/router';
 import { DayCardComponent } from './day-card/day-card.component';
 import { EditDayComponent } from './edit-day/edit-day.component';
 import { NewDayComponent } from './new-day/new-day.component';
+import { LoginComponent } from './pages/login/login.component';
+import { EditAccountComponent } from './edit-account/edit-account.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 
 export const routes: Routes = [
 
+    {
+        path: '',
+        component: LoginComponent,
+    },
     {
         path: 'home',
         component: DayCardComponent,
@@ -21,6 +28,21 @@ export const routes: Routes = [
         path: 'add',
         component: NewDayComponent,
         title: 'add'
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        title: 'login'
+    },
+    {
+        path: 'my-account',
+        component: EditAccountComponent,
+        title: 'my account'
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+        title: 'register'
     }
 
 ];
