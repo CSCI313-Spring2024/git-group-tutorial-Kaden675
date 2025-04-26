f5512025eb59463b0532e3d89d2abd05da32792e
Luke Ness <lukedness@gmail.com>
2025-04-26 16:01:24 -0500

    added in new-day and edit-day components

---
+++ not-so-fit-bit/src/app/new-day/new-day.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-day',
+  imports: [],
+  templateUrl: './new-day.component.html',
+  styleUrl: './new-day.component.css'
+})
+export class NewDayComponent {
+
+}
