9cc88eb34554aeef7f31b6e02508d217f8ec33f8
Luke Ness <lukedness@gmail.com>
2025-04-26 15:21:18 -0500

    added in interface Day into day.service.ts to have a structure of what we are using

--- not-so-fit-bit/src/app/day.service.ts
+++ not-so-fit-bit/src/app/day.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+
+export interface Day {
+  id: number,
+  date: string,
+  workout: string,
+  exercise: string,
+  fluid: number,
+  calorie: number,
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class DayService {
 
   constructor() { }
