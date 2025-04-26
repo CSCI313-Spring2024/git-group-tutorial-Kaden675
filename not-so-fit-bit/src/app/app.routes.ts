bdaf9f92e235ac5c4adc97db0dc17051606a8f5e
Luke Ness <lukedness@gmail.com>
2025-04-26 16:10:19 -0500

    added in day card component and modified new-day component

--- not-so-fit-bit/src/app/app.routes.ts
+++ not-so-fit-bit/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DayCardComponent } from './day-card/day-card.component';
+import { EditDayComponent } from './edit-day/edit-day.component';
+import { NewDayComponent } from './new-day/new-day.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+
+{
+
+}
+
+];
--- not-so-fit-bit/src/app/new-day/new-day.component.ts
+++ not-so-fit-bit/src/app/new-day/new-day.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DayService } from '../day.service';
+import { Day } from '../day.service';
 
 @Component({
   selector: 'app-new-day',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './new-day.component.html',
   styleUrl: './new-day.component.css'
 })
 export class NewDayComponent {
 
+  date= "";
+  workout= "";
+  exerciseDuration= "";
+  fluid= "";
+  calorie="";
+
+  constructor(
+    private dayService: DayService,
+    private router: Router
+  ){}
+
+  onSubmit(){
+    this.dayService.addDay({
+      date: this.date,
+      workout: this.workout,
+      exerciseDuration: this.exerciseDuration,
+      fluid: this.fluid,
+      calorie: this.calorie
+    }).subscribe(() => this.router.navigate(['home']))
+  }
+
+  onCancel(){
+    this.router.navigate(['home'])
+  }
 }
