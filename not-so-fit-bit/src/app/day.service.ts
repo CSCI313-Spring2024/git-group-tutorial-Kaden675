95bb7414fae0353dfc54f82237cc2e0490b50072
Luke Ness <lukedness@gmail.com>
2025-04-26 15:32:53 -0500

    added methods to the day service to have a basis of adding, deleting, editing, and getting days

--- not-so-fit-bit/src/app/day.service.ts
+++ not-so-fit-bit/src/app/day.service.ts
@@ -5,8 +5,8 @@ export interface Day {
   id: number,
   date: string,
   workout: string,
-  exercise: string,
-  fluid: number,
+  exerciseDuration: string,
+  fluid: string,
   calorie: number,
 }
 
@@ -17,5 +17,43 @@ export interface Day {
 
 export class DayService {
 
+  private Days: Day[] = [
+    {
+      id: 99999,
+      date: "April 26",
+      workout: "bench",
+      exerciseDuration: "1 hour",
+      fluid: "1 liter",
+      calorie: 2000 
+    },
+
+]
+
+private nextId = this.Days.length;
+
   constructor() { }
+
+  getDays(){
+    return of([...this.Days]);
+  }
+
+  getDayById(id: number){
+    return of(this.Days.find( Days => Days.id === id)!);
+  }
+
+  addDay(day: Omit<Day, 'id'>){
+    const newDay = {...day, id: this.nextId++};
+    this.Days.push(newDay);
+    return of(newDay)
+  }
+  updateDay(id: number, updated: Partial<Day>){
+    const index = this.Days.findIndex( days => days.id === id);
+    if(index !== -1){
+      this.Days[index]={...this.Days[index], ...updated};
+    }
+  }
+  deleteDay(id:number){
+    this.Days = this.Days.filter(Days=>Days.id !== id);
+    return of(true)
+  }
 }
