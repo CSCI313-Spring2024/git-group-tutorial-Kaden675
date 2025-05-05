01604a6db40b0db086b6000645d9d4529640800c
Luke Ness <lukedness@gmail.com>
2025-05-04 20:28:27 -0500

    added in home stuff...kinda working

--- not-so-fit-bit/src/app/pages/home/home.component.ts
+++ not-so-fit-bit/src/app/pages/home/home.component.ts
@@ -1,9 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../auth.service';
+import { Input } from '@angular/core';
+import { Day } from '../../day.service';
+import { Router, RouterLink } from '@angular/router';
+import { DayService } from '../../day.service';
+import { CommonModule } from '@angular/common';
+import { NewDayComponent } from '../../new-day/new-day.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink, CommonModule, NewDayComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -12,4 +18,23 @@ export class HomeComponent {
   private authService = inject(AuthService);
 
   user = this.authService.getUser();
+  
+  days: Day[] = [];
+  
+    trackById(index: number, item: any): any {
+      return item.id;
+    }
+  
+    
+    constructor(private daysService: DayService){
+      this.loadDays()
+    }
+  
+    loadDays(){
+      this.daysService.getDays().subscribe(data => this.days = data)
+    }
+  
+    deleteDay(id:number){
+      this.daysService.deleteDay(id).subscribe(() => this.loadDays)
+    }
 }
